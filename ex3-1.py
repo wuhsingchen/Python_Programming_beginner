4867fb9282fdd8fc781ee01c5cd4ec9eafe033e5
wuhsingchen <wuhsingchen317@gmail.com>
2025-04-01 19:40:47 +0800

    Update ex3-1.py

--- ex3-1.py
+++ ex3-1.py
@@ -1,3 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 name= "吳幸蓁"
 age=14
-print( name, age)-
+print( name, age)
+
+x, y, z =1, 2, 3
+print(x, y, z)
+
+x =10       #int
+y =3.14     #floot
+z ="Hello"  #str
+print(type(x), type(y), type(size_t))
