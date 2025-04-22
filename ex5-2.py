3327e9658f1a66b1e95aaa4a5b2e7b991c6c83e2
wuhsingchen <wuhsingchen317@gmail.com>
2025-04-22 21:07:51 +0800

    Update ex5-2.py

--- ex5-2.py
+++ ex5-2.py
@@ -3,3 +3,27 @@ y = int(input("please input a num :"))
 print("x = ",x)
 print("y = ",y)
 print("x + y =",x+y)
+
+
+
+
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+'''
+2.輸入一個圓半徑，計算並輸出圓的面積和周長  面積 =  *r*r, 周長 =2*pi *r
+
+x = int(input("please input a num :"))
+y = int(input("please input a num :"))
+r = int(input("please input a num radius  :"))
+print("x = ",x)
+print("y = ",y)
+print("r = ",r)
+print("x + y=",x+y)
+print("面積 = "3.14*r*r)
+print(" 周長 ="2*3.14*r)
